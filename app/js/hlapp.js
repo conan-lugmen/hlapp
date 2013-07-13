9ea007fcba7edfd16ef4bfb37f6ba267ed507074
conan (aka Fernando Canizo) <conan@lugmen.org.ar>
2013-07-13 14:56:04 -0300

    Rename myApp -> hlApp
    Also rename files.
    Add Lore Ipsum view to be used on hlApp.

---
+++ app/js/hlapp.js
@@ -0,0 +1,15 @@
+// Creation Date: 13 Jul 2013
+// Author: Fernando Canizo (aka conan) - http://conan.muriandre.com/
+
+
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('hlApp', ['hlApp.controllers'])
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.when('/hlapp', {templateUrl: 'partials/hlapp.html', controller: 'hlAppCtrl'});
+		$routeProvider.when('/config', {templateUrl: 'partials/config.html', controller: 'configCtrl'});
+
+		$routeProvider.otherwise({redirectTo: '/hlapp'});
+	}]);
